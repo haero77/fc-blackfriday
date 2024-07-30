d9eb40990e0872a09420368790686c0a3b86ca74
haero77 <shineecard5@gmail.com>
2024-07-30 20:20:46 +0900

    initial commit

---
+++ src/main/java/org/example/blackfriday/BlackfridayApplication.java
@@ -0,0 +1,12 @@
+package org.example.blackfriday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackfridayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackfridayApplication.class, args);
+	}
+}
---
+++ src/test/java/org/example/blackfriday/BlackfridayApplicationTests.java
@@ -0,0 +1,12 @@
+package org.example.blackfriday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackfridayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
