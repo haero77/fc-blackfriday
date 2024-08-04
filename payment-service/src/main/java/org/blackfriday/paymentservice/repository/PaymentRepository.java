783d270ae630dd4b65381d8bf5f36f05f9676c8e
haero77 <shineecard5@gmail.com>
2024-08-04 21:26:43 +0900

    chore(yml): add datasource

--- payment-service/src/main/java/org/blackfriday/paymentservice/repository/PaymentRepository.java
+++ payment-service/src/main/java/org/blackfriday/paymentservice/repository/PaymentRepository.java
@@ -1,10 +1,8 @@
 package org.blackfriday.paymentservice.repository;
 
 import org.blackfriday.paymentservice.entity.Payment;
-import org.blackfriday.paymentservice.entity.PaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-    PaymentMethod findByUserId();
 }
