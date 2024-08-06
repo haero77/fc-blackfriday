8d94400fcf228d28805c396bdf2cd39a8fb02df6
haero77 <shineecard5@gmail.com>
2024-08-06 20:12:21 +0900

    (delivery) initial commit

---
+++ delivery-service/src/test/java/org/blackfriday/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package org.blackfriday.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
